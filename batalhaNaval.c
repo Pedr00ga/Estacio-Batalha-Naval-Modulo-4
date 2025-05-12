9782efa63e5e811bc77bac8982ffa9e65da336e0
Pedr00ga <plgonzales4760@gmail.com>
2025-05-12 11:23:46 -0300

    Desafio novato com comentarios atualizados

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 
 int main(){
-
+    //Cria o tabuleiro 10x10 com todas as posicoes zeradas
     int tabuleiro[10][10] = {0};
 
-    int navioH[3] = {3, 3, 3}; // Navios horizontais
-    int navioV[3] = {3, 3, 3}; // Navios verticais
+    int navioH[3] = {3, 3, 3}; // Navios array do navio horizontal
+    int navioV[3] = {3, 3, 3}; // Navios array do navio vertical
 
+    // Define posicionamento do navio horizontal
     int linhaH = 2, colunaH = 3;
     for (int i = 0; i < 3; i++) {
         tabuleiro[linhaH][colunaH + i] = navioH[i];
     }
     
-    // 4. Posicionamento do navio vertical (linha 5, coluna 7)
+    //Define posicionamento do navio horizontal
     int linhaV = 5, colunaV = 7;
     for (int i = 0; i < 3; i++) {
         tabuleiro[linhaV + i][colunaV] = navioV[i];
     }
 
+    // Faz o tratamento de erros para verificar se os navios estão fora do tabuleiro ou colidiram
     if (linhaV < 0 || linhaV >= 10 || colunaV < 0 || colunaV >= 10) {
         printf("Posicao invalida para o navio vertical.\n");
         return 1;
@@ -32,7 +34,8 @@ int main(){
         return 1;
     }
 
-    printf("Tabuleiro inicial:\n");
+    // Mostra o tabuleiro com o navios posicionados
+    printf("Tabuleiro:\n");
     for(int i = 0; i < 10; i++){
         for(int j = 0; j < 10; j++){
             printf("%d ", tabuleiro[i][j]);
